4b9aa7454b5df64e5ece250d853f36c9634fe577
Kevin <kevinbowe1999@gmail.com>
2020-01-20 15:04:06 -0500

    Create Stateful Widget that changes Text when button is pushed

--- lib/main.dart
+++ lib/main.dart
@@ -4,21 +4,38 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State{
+  var questionIndex = 0;
+  
   void answerQuestion(){
+    setState(() {
+      questionIndex++;
+    });
     print('Answered Question');
   }
 
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite country in the list bellow?',
+      'What\'s your favorite actor?',
+      'What\'s your favorite animal'
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text('My First App'),
         ),
         body: Column(
           children: <Widget>[
-            Text('The Question'),
-            RaisedButton(child: Text('Answer 1'), onPressed: () => print('Answer 1 Chosen'),),
+            Text(questions[questionIndex]),
+            RaisedButton(child: Text('Answer 1'), onPressed: answerQuestion,),
             RaisedButton(child: Text('Answer 2'), onPressed: answerQuestion,),
             RaisedButton(child: Text('Answer 3'), onPressed: answerQuestion,),
             RaisedButton(child: Text('Answer 4'), onPressed: answerQuestion,),
